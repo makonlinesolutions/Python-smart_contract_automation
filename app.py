567039e9dee086d15c57350ac3ce83478d19cea9
makonlinesolutions <makonlinesolutions@gmail.com>
2025-05-20 05:43:49 +0530

    Minor Adjustments for API

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 from automation.run_master import run_master
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route("/api/start", methods=["POST"])
 def start():
